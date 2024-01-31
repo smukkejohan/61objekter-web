8179b8f1b63715c10cb6a9788b602af0c5a0b2d6
Johan Bichel <public@johan.cc>
2024-01-31 22:44:10 +0100

    some content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>61 Objekter</h1>
+<p>Workshop collective in Copenhagen</p>
+
+<address>
+    Prags Boulevard 61<br>
+    2300 København S<br>
+    Denmark
+</address>
+
+
+<h2>We are:</h2>
+<ul>
+    <li><a href="https://www.mssnedkeri.com/">Mikkel Søe Snedkeri</a></li>
+    <li><a href="https://www.instagram.com/ballowitzguitars/">Ballowitz Guitars</a></li>
+    <li><a href="https://www.temporal.dk">Temporal</a></li>
+    <li><a href="">Jens Erik Larsen</a></li>
+    <li>Pil</li>
+    <li>Anna</li>
+    <li>Frederik</li>
+    <li><a href="https://www.johan.cc">Johan.cc</a></li>
+
+    <li>Batch Productions</li>
+</ul>
+and more
+
+
+
+<h2>Contact</h2>
+<p>For inquiries, please contact <a href="mailto:public@johan.cc">public@johan.cc</a></p>
