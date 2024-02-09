4bfc57ba9ae45811858473999db8ae95e0b9c68d
Johan Bichel <public@johan.cc>
2024-02-09 17:58:38 +0100

    update yues

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+    import "../app.css";
+    const year = new Date().getFullYear();
+</script>
+
+<div class="container mx-auto">
+    <header class="mt-4">    
+      <h1 class="text-6xl">61 Objekter</h1>
+      <p class="text-2xl mt-4">Workshop collective in Copenhagen</p>
+
+</header>
+<main class="mt-4">
+  <slot />
+</main>
+
+<footer>
+
+    <address>
+      Prags Boulevard 61<br>
+      2300 København S<br>
+      Denmark
+    </address>
+
+  <p>© {year} 61 Objekter</p>
+</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,52 @@
-<h1>61 Objekter</h1>
-<p>Workshop collective in Copenhagen</p>
-
-<address>
-    Prags Boulevard 61<br>
-    2300 København S<br>
-    Denmark
-</address>
+<script lang="ts">
+    import InstagramIcon from 'virtual:icons/mdi/instagram'
+    import SiteIcon from 'virtual:icons/mdi/web'
 
 
-<h2>We are:</h2>
-<ul>
-    <li><a href="https://www.mssnedkeri.com/">Mikkel Søe Snedkeri</a></li>
-    <li><a href="https://www.instagram.com/ballowitzguitars/">Ballowitz Guitars</a></li>
-    <li><a href="https://www.temporal.dk">Temporal</a></li>
-    <li><a href="">Jens Erik Larsen</a></li>
-    <li>Pil</li>
-    <li>Anna</li>
-    <li>Frederik</li>
-    <li><a href="https://www.johan.cc">Johan.cc</a></li>
+    const people : {company?: string, person?: string, site?: string, ig?: string, email?: string, phone?: string }[] = [
+        { company: 'Mikkel Søe Snedkeri', person: 'Mikkel Søe', site: 'https://www.mssnedkeri.com/', ig: 'mssnedkeri'},
+        { company: 'Ballowitz Guitars', person: 'Henrik Guitars', ig: 'ballowitzguitars'},
+        { company: 'Temporal', person: 'Johan Bichel Lindegaard', site: 'https://www.temporal.dk', ig: 'studio_temporal' },
+        { company: 'JEL Instruments', person: 'Jens Erik Larsen', site: 'https://www.jelinstruments.com/',  ig: 'jel_instrumenter'},
+        { company: 'Hysteria Objects', person: 'Pil Whitta', site: 'https://www.hysteria-objects.com/', ig: 'hysteria_objects' },
+        { person: 'Anna Søgaard', ig: 'soegaardanna' },
+        { company: '', person: 'Frederik Friberg', site: '', ig: '_moebel_' },
+        { company: 'Johan.cc', person: 'Johan.cc', site: 'https://www.johan.cc' },
+        { company: 'Batch Productions', site: 'https://www.batchproductions.dk/', ig: 'batch_productions', email: 'hello@batchproductions.dk' },
+    ]
 
-    <li>Batch Productions</li>
+</script>
+
+<ul class="grid grid-rows-3 grid-flow-col gap-4">
+    {#each people as person}
+        <li>
+            {#if person.company}
+            <h2 class="text-4xl">{person.company}</h2>
+
+            {:else}
+                {#if person.person }
+                    <h2 class="text-4xl">{person.person}</h2>
+                {/if}
+            {/if}
+            <div class="flex">
+                {#if person.site}
+                <a href={person.site} title="Website for {person.company || person.person}"><SiteIcon/></a>
+            {/if}
+            {#if person.ig}
+                <!-- instragram icon -->
+                <a href="https://www.instagram.com/{person.ig}/"><InstagramIcon class="w-6 h-6" /></a>
+            {/if}
+            {#if person.email}
+                <a href="mailto:{person.email}">{person.email}</a>
+            {/if}
+            {#if person.phone}
+                <a href="tel:{person.phone}">{person.phone}</a>
+            {/if}
+            </div>
+            
+        </li>
+    {/each}
 </ul>
-and more
-
-
 
 <h2>Contact</h2>
 <p>For inquiries, please contact <a href="mailto:public@johan.cc">public@johan.cc</a></p>
